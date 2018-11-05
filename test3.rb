81486334794e499a88d84149828a41dc08f6626c
krenoirw <ein-reno-flower@outlook.jp>
2018-11-05 18:21:43 +0900

    Add files via upload

--- test3.rb
+++ test3.rb
@@ -1,35 +1,34 @@
 module Test3
-	extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-	included do
-		before_save :write_log
+  included do
+    before_save :write_log
 
-		def write_log; end
-	end
+    def write_log; end
+  end
 
-	class_methods do
-		def model_log(&block)
-			define_method(:write_log) do
-			  stop_watch do
-			    instance_exec(&block) if block_given?
-			  end
-			end
-		end
+  class_methods do
+    def model_log(&block)
+      define_method(:write_log) do
+        stop_watch do
+          instance_exec(&block) if block_given?
+        end
+      end
+    end
 
-		# class User < ApplicationRecord
-		#   include Test3
+    # class User < ApplicationRecord
+    #   include Test3
+    #   model_log do
+    #     Rails.logger.info("Hello World")
+    #   end
+    # end
+  end
 
-		# 	model_log do
-		# 		Rails.logger.info("Hello World")
-		# 	end
-		# end
-	end
-
-	def stop_watch
-		Rails.logger.info("----- start -----")
-		start_time = Time.zone.now
-		yield
-		end_time = Time.zone.now
-		Rails.logger.info("----- finished #{end_time - start_time}.seconds -----")
-	end
+  def stop_watch
+    Rails.logger.info("----- start -----")
+    start_time = Time.zone.now
+    yield
+    end_time = Time.zone.now
+    Rails.logger.info("----- finished #{(end_time - start_time).to_i}.seconds -----")
+  end
 end
